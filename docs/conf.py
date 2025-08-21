33f794a3d51f357228ee5f686569f9865174667b
Antonio M. Ferreira, Ph.D <amferreira68@gmail.com>
2025-08-21 18:58:41 -0400

    Working Sphinx config with Mermaid figures for flowcharts

--- docs/conf.py
+++ docs/conf.py
@@ -13,12 +13,15 @@ extensions = [
 ]
 
 # Set up theme
-html_theme = "furo"
+html_theme = "nature"
+html_theme_options = {
+    "sidebarwidth": "24em",   # widen the sidebar to accommodate long function names
+}
 
 # Autodoc settings
 autodoc_typehints = "description"
 
 # Mermaid settings
-mermaid_version = "10.3.0"
+mermaid_version = "11.2.0"
+mermaid_d3_zoom = True
 mermaid_init_js = "mermaid.initialize({startOnLoad:true});"
-mermaid_params = ['-p', 'puppeteer-config.json']
